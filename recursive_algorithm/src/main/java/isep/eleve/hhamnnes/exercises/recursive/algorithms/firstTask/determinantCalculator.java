08bbb63b65d0329caf0f38301089ecd1c8ede522
hhamnnes <96078953+hhamnnes@users.noreply.github.com>
2025-03-08 19:42:39 +0100

    Added more code to the determinantCalculator

--- recursive_algorithm/src/main/java/isep/eleve/hhamnnes/exercises/recursive/algorithms/firstTask/determinantCalculator.java
+++ recursive_algorithm/src/main/java/isep/eleve/hhamnnes/exercises/recursive/algorithms/firstTask/determinantCalculator.java
@@ -12,18 +12,29 @@ public class determinantCalculator {
             throw new IllegalArgumentException("The matrix must be a square");
         }
 
-        
+        if(numberOfColumns == 1) {
+            return a[0][0];
+        }
 
-        
+        else {
+
+            for (int i = 0, j = 0; i < numberOfColumns; i++, j++){
+
+                
+
+            }
+
+        }
 
     }
 
     public static boolean squareMatrix(int numberOfColumns,
                                        int numberOfRows) {
-        int sideLength = numberOfColumns + numberOfRows;
 
-        if(sideLength % 2 == 0) return true;
+        if(numberOfRows % 2 == 0 && numberOfColumns == numberOfRows) return true;
         else return false;
+
+        if()
 
     }
     
