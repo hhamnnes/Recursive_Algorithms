2ab662e38e86cd9683633dcef280bcc66d6f45a5
hhamnnes <96078953+hhamnnes@users.noreply.github.com>
2025-03-08 09:38:20 +0100

    secound commit

---
+++ recursive_algorithm/src/main/java/isep/eleve/hhamnnes/exercises/recursive/aglorithms/Main.java
@@ -0,0 +1,7 @@
+package isep.eleve.hhamnnes.exercises.recursive.aglorithms;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
